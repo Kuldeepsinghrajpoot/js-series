c5d0a1e7f1f9871e70e832012039174704e104a6
Kuldeep Singh Rajpoot <kuldeepsinghrajpoot4@gmail.com>
2024-07-15 15:29:38 +0530

    feat: Add variable declaration examples using var, let, and const
    This commit adds examples of variable declaration using var, let, and const in JavaScript. It explains the differences between var, let, and const in terms of scope, redeclaration, and updating. It also demonstrates how to declare variables without assigning a value to them.

---
+++ basic/variables.js
@@ -0,0 +1,41 @@
+// lets begin
+
+// first we need to declare a variable
+
+// var ke bare ,
+// var is a keyword used to declare a variable in javascript 
+// var is function scoped or a global scoped es ko nahi padna hai abhi thoda sa wait karo m bata dunga latter
+
+// var can be redeclared and updated
+// var can be declared without assigning a value to it
+
+var myVariable = 10;  // myvariable eak name h jo ki values ko hold karta h , or yah var type ka h
+var myVariable = 20;
+var myVariable
+
+console.log(myVariable); // 10
+
+// we can also declare a variable without assigning a value to it
+// let ko eak bar declare karne ke bad dubara declare nahi kar sakte
+// let is block scoped
+// let is a keyword used to declare a variable in javascript
+// let can't be redeclared but can be updated
+{ let isUserLoggedIn }
+
+{ let isUserLoggedIn = true; }
+
+{ let isUserLoggedIn = false; }
+
+// same variable name could't assign in the let
+let headmaster = 'akalesh';
+ headmaster = 'kuldeep';
+
+
+
+// cosnt is blocked scoped
+// const is a keyword used to declare a variable in javascript
+// const can't be redeclared or updated
+// const must be assigned a value at the time of declaration
+const myConstVariable;
+const name = 'hi';
+
